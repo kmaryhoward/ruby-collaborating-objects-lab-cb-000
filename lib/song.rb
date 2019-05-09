a2962384521e43213000b813901a0cf68803d9ee
Kathleen Howard <kmaryhoward@gmail.com>
2019-05-09 14:58:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     song = self.new(song_name)
-    
+    song.artist_name(filename.split(" - ")[0])
   end
 
 
